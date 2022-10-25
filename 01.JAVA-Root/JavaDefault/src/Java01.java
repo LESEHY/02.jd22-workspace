8e9c1bc2a982cc0d9b88980fcd3d2d11a0fbce5c
LESEHY <tmdgus2496@gmail.com>
2022-10-25 16:18:05 +0900

    ss

---
+++ 01.JAVA-Root/JavaDefault/src/Java01.java
@@ -0,0 +1,35 @@
+public class Java01 {
+    public static void main(String[] args) {
+        // 자바 클래스는 소스 단위블럭
+        // 자바 main 메서드는 클래스의 첫 실행 메서드
+        // 자바한줄주석
+        /* 여러줄주석 */
+
+        // 자바 출력 1 : println() 
+        // -> 뒤에 엔터기호가 들어가 있는 출력 메서드
+        // 단축키 : syso 
+        System.out.println("자바구문을 알자!");
+        // 자바 출력 2 : print()
+        // -> 뒤에 엔터기호가 없어서 한줄에 나옴!
+        System.out.print("나");
+        System.out.print("야");
+        System.out.print("나");
+        System.out.println("너두냐");
+        // 홑따옴표 -> 한 글자만 출력됨!
+        System.out.println('홑');
+        // System.out.println('홑따옴표');
+        
+        // 숫자출력
+        System.out.println(3);
+        System.out.println(30);
+        System.out.println(365);
+        System.out.println(3650);
+        System.out.println(36500);
+
+        // 숫자계산
+        System.out.println(10+2);
+        System.out.println(10-2);
+        System.out.println(10*2);
+        System.out.println(10/2);
+    }
+}
