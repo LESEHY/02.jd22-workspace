97135f8b7460a7058d20e3caf9b2d4a2068030fa
LESEHY <tmdgus2496@gmail.com>
2022-11-03 11:22:43 +0900

    ss

--- 01.JAVA-Root/JavaDefault/src/Java04.java
+++ 01.JAVA-Root/JavaDefault/src/Java04.java
@@ -161,8 +161,29 @@ public class Java04 {
             // // 실행코드
             // }
             // while (조건문);
-            
 
+            // 자신의 월급을 쓰고 몇번 월급받고 싶은지 써서
+            // 월급의 총액을 알아보자!
+            System.out.println("당신의 월급은 얼마입니까?");
+            int myPay = myObj.nextInt();
+            System.out.println("월급: " + myPay+"원");
+            System.out.println();
+
+            System.out.println("당신은 당신의 회사에서 몇번이나 월급을 받고 싶나요?");
+            int payNum = myObj.nextInt();
+            System.out.println("월급횟수: " + payNum);
+            System.out.println();
+
+            // 월급횟수 증가변수
+            int payCycle = 1;
+            do{
+                System.out.println("월급 " + myPay + "원 *"+
+                payCycle+"번 받고 \n 총액: "+
+                (myPay*payCycle)+"원");
+
+                // 증감필수!!!
+                payCycle++;
+            } while(payCycle <= payNum); // ";" 필수
         } // try문 (스캐너)
 
         // catch문 (에러발생시 처리 구역)
